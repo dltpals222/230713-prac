1ea553c5d23d075bde1d2538eb84bf12b56b5c1c
BlahBlah <leesemin222@gmail.com>
2023-07-13 10:57:28 +0900

    react, react-dom 설치

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ class Server {
             res.end();
           }
         });
+        fs.readFile(path.join(__dirname, "react.js"), (err, data) => {
+          if (err) {
+            res.writeHead(500, { "Content-Type": "text/html" });
+            res.write("500");
+            res.end();
+          } else {
+            res.writeHead(200, { "Content-Type": "text/javascript" });
+            res.write(data);
+            res.end();
+          }
+        });
       })
       .listen(this.port, (err) => {
         if (err) {
---
+++ react.js
@@ -0,0 +1,14 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+const app = () => {
+  return (
+    <div>
+      <h1>
+        나는 신발이 없음을 한탄했는데, <br /> 길에서 발이 없는 사람을 만났다.
+      </h1>
+    </div>
+  );
+};
+
+ReactDOM.render(app(), document.getElementById("root"));
